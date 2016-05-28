3d85f95f85fca21a9514a701321987b00a4c978d
David Sverdlov <dsverdlo@vub.ac.be>
2016-05-29 00:03:20 +0200

    Update disable_votes
    Made it an iouplugin and added extra cvar for extra restrictions for
    spectators

--- disable_votes.py
+++ disable_votes.py
@@ -8,66 +8,90 @@
 # This plugin disables certain votes during a game
 #
 # Uses
-# - qlx_disabled_votes_midgame "map, teamsize"
-# - qlx_disabled_votes_permission "1"
+# set qlx_disabled_votes_midgame "map"
+# set qlx_disabled_specvotes_midgame "teamsize, kick, clientkick"
+# set qlx_disabled_votes_permission "1"
 
 import minqlx
 import time
 import os
 import requests
 
-VERSION = "v0.10"
+VERSION = "v0.11"
 
+# This code makes sure the required superclass is loaded automatically
+try:
+    from .iouonegirl import iouonegirlPlugin
+except:
+    try:
+        abs_file_path = os.path.join(os.path.dirname(__file__), "iouonegirl.py")
+        res = requests.get("http://wilma.vub.ac.be/~dsverdlo/minqlx-plugins/iouonegirl.py")
+        if res.status_code != requests.codes.ok: raise
+        with open(abs_file_path,"a+") as f: f.write(res.text)
+        from .iouonegirl import iouonegirlPlugin
+    except Exception as e :
+        minqlx.CHAT_CHANNEL.reply("^1iouonegirl abstract plugin download failed^7: {}".format(e))
+        raise
 
-class disable_votes(minqlx.Plugin):
+class disable_votes(iouonegirlPlugin):
 
     def __init__(self):
-        self.set_cvar_once("qlx_disabled_votes_midgame", "map, teamsize")
+        super().__init__(self.__class__.__name__, VERSION)
+
+        self.set_cvar_once("qlx_disabled_votes_midgame", "map")
+        self.set_cvar_once("qlx_disabled_specvotes_midgame", "teamsize, kick, clientkick")
         self.set_cvar_once("qlx_disabled_votes_permission", "1")
 
         self.add_hook("vote_called", self.handle_vote)
-        self.add_hook("player_connect", self.handle_player_connect)
-        self.add_command("v_disable_votes", self.cmd_version)
+        self.add_command(("disabled","disabled_votes"), self.cmd_disabled)
+
+    def cmd_disabled(self, player, msg, channel):
+        disabled_for_all = []
+        for v in self.get_cvar("qlx_disabled_votes_midgame", set):
+            disabled_for_all.append(v)
+
+        disabled_for_spec = []
+        for v in self.get_cvar("qlx_disabled_specvotes_midgame", set):
+            disabled_for_spec.append(v)
+
+        if disabled_for_all:
+            m = "^7Unless perm ^6{}^7+, in-game votes for ^2{}^7 are disabled."
+            m = m.format(self.get_cvar("qlx_disabled_votes_permission"), "^7, ^2".join(disabled_for_all))
+            if disabled_for_spec:
+                m += " Specs can also not vote for: ^2{}^7."
+                m = m.format("^7, ^2".join(disabled_for_spec))
+
+        elif disabled_for_spec:
+            m = "Unless they have perm ^6{}^7+, specs can't vote for: ^2{}^7."
+            m = m.format(self.get_cvar("qlx_disabled_votes_permission"), "^7, ^2".join(disabled_for_spec))
+        else:
+            m = "^7There are no callvotes that will be disabled during a match."
+
+        channel.reply(m)
 
 
     def handle_vote(self, player, vote, args):
 
         if self.game.state != "in_progress": return
 
+        if self.db.has_permission(player, self.get_cvar("qlx_disabled_votes_permission", int)):
+            return # go ahead
+
+        disabled_votes = []
+
+        # These will always apply
         for v in self.get_cvar("qlx_disabled_votes_midgame", set):
-            if v == vote and not self.db.has_permission(player, self.get_cvar("qlx_disabled_votes_permission")):
-                self.msg('^1You are not allowed to callvote {} during a match!'.format(v))
-                return minqlx.RET_STOP_ALL
+            disabled_votes.append(v)
+
+        # If spectator: extra restrictions
+        if player.team == "spectator":
+            for v in self.get_cvar("qlx_disabled_specvotes_midgame", set):
+                disabled_votes.append(v)
+
+        if vote in disabled_votes:
+            self.msg('^1You are not allowed to callvote {} during a match!'.format(vote))
+            return minqlx.RET_STOP_ALL
 
 
-    def handle_player_connect(self, player):
-        if self.db.has_permission(player, 5):
-            self.check_version(player=player)
 
-    def cmd_version(self, player, msg, channel):
-        self.check_version(channel=channel)
-
-    @minqlx.thread
-    def check_version(self, player=None, channel=None):
-        url = "https://raw.githubusercontent.com/dsverdlo/minqlx-plugins/master/{}.py".format(self.__class__.__name__)
-        res = requests.get(url)
-        last_status = res.status_code
-        if res.status_code != requests.codes.ok: return
-        for line in res.iter_lines():
-            if line.startswith(b'VERSION'):
-                line = line.replace(b'VERSION = ', b'')
-                line = line.replace(b'"', b'')
-                # If called manually and outdated
-                if channel and VERSION.encode() != line:
-                    channel.reply("^7Currently using ^3iou^7one^4girl^7's ^6{}^7 plugin ^1outdated^7 version ^6{}^7.".format(self.__class__.__name__, VERSION))
-                # If called manually and alright
-                elif channel and VERSION.encode() == line:
-                    channel.reply("^7Currently using ^3iou^7one^4girl^7's latest ^6{}^7 plugin version ^6{}^7.".format(self.__class__.__name__, VERSION))
-                # If routine check and it's not alright.
-                elif player and VERSION.encode() != line:
-                    time.sleep(15)
-                    try:
-                        player.tell("^3Plugin update alert^7:^6 {}^7's latest version is ^6{}^7 and you're using ^6{}^7!".format(self.__class__.__name__, line.decode(), VERSION))
-                    except Exception as e: minqlx.console_command("echo {}".format(e))
-                return
 
