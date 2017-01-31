f1b2a8d13da8c919eb67e096c04215e6888f7111
abighairyspider <jwinett@users.noreply.github.com>
2017-01-30 22:26:23 -0600

    qlx_intermission_songs may contain comma-delimited list of songs
    Server operator no longer has to edit the plugin after an update when using qlx_intermission_songs, but can edit and populate SONGS if preferred.  Also not running intermission if the game was aborted, and added !intermission for admin to test song list.

--- intermission.py
+++ intermission.py
@@ -1,5 +1,5 @@
 # This is a plugin created by iouonegirl(@gmail.com)
-# Copyright (c) 2016 iouonegirl
+# Copyright (c) 2016,2017 iouonegirl
 # https://github.com/dsverdlo/minqlx-plugins
 #
 # You are free to modify this plugin to your custom,
@@ -18,39 +18,65 @@ import minqlx
 import time
 import requests
 
-VERSION = "v0.20"
+VERSION = "v0.30"
 
 # These songs will be looped one by one. Don't forget to remove the #'s if you want to use songs
+# or set the cvar qlx_intermission_songs to a comma-delimited list of songs in server.cfg, like:
+# set qlx_intermission_songs "sound/songname/songtitle1.ogg,sound/songname/songtitle2.ogg,sound/songname/songtitle3.ogg"
+
 SONGS = [
-    #"sound/songname/songtitle.ogg",
-    #"sound/songname/songtitle.ogg",
-    #"sound/songname/songtitle.ogg",
+    #"sound/songname/songtitle1.ogg",
+    #"sound/songname/songtitle2.ogg",
+    #"sound/songname/songtitle3.ogg",
 ]
+
+# When qlx_intermission_songs has been set, then the value of SONGS is ignored.
+
 
 class intermission(minqlx.Plugin):
     def __init__(self):
         self.index = 0
 
+        self.set_cvar_once( "qlx_intermission_songs", ",".join(SONGS)  )
+        self.songs = self.get_cvar( "qlx_intermission_songs", list )
+
+        if not self.songs:
+            self.msg("^1Error: No song list for intermission.^7")
+            return
+
         self.add_hook("game_end", self.handle_game_end)
         self.add_hook("player_connect", self.handle_player_connect)
         self.add_command("v_intermission", self.cmd_version)
+        self.add_command("intermission", self.cmd_intermission, 3 )
+
+    def cmd_intermission( self, player, msg, channel):
+        for p in self.players():
+            if self.db.get_flag(p, "essentials:sounds_enabled", default=True):
+                self.stop_sound(p)
+        self.handle_game_end( { "ABORTED":0 }  )
+
+        x = 0
+        for x, song in enumerate(self.songs):
+            channel.reply(  "{}{}".format( song, " (now playing)" if x == self.index else "" ) )
+            x += 1
+
 
     @minqlx.delay(0.3)
-    def handle_game_end(self, *args, **kwargs):
-
+    def handle_game_end(self, data ):
         # If there are no songs defined, return
-        if not SONGS: return
+        if not self.songs or data["ABORTED"]:
+            return
 
         # If last time the index was incremented too high, loop around
-        if self.index == len(SONGS):
+        if self.index >= len(self.songs):
             self.index = 0
 
         # Try to play sound file
-        try:            
+        try:
             for p in self.players():
                 if self.db.get_flag(p, "essentials:sounds_enabled", default=True):
                     self.stop_music(p)
-                    self.play_sound(SONGS[self.index],p)
+                    self.play_sound(self.songs[self.index],p)
         except Exception as e:
             self.msg("^1Error: ^7{}".format(e))
 
